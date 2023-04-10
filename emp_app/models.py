42ab2bddcb468248a113f4dc99fcc87cf20cb45d
shrutiso05 <shristisoni051199@gmail.com>
2023-04-10 10:38:25 +0530

    add files

--- emp_app/models.py
+++ emp_app/models.py
@@ -5,9 +5,13 @@ class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 class Role(models.Model):
     name = models.CharField(max_length=100, null=False)
-
+    
+    def __str__(self):
+        return self.name
 class Employee(models.Model):
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100)
@@ -15,5 +19,8 @@ class Employee(models.Model):
     salary = models.IntegerField(default=0)
     bonus = models.IntegerField(default=0)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    Phone = models.IntegerField(default=0)
+    phone = models.IntegerField(default=0)
     hire_date = models.DateField()
+
+    def __str__(self):
+        return "%s %s %s" %(self.first_name, self.last_name,self.phone)
